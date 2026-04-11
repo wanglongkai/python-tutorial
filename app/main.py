f1e463c1c12a20cb6cf5634120a68cd68415aeb9
wanglongkai123 <your.email@example.com>
2026-04-11 22:15:36 +0800

    feat: 添加CORS中间件以支持跨域请求

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from router.userRouter import user_router
 from router.newsRouter import news_router
 from db.dbconfig import handle_tortoise
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router)
 app.include_router(news_router)
